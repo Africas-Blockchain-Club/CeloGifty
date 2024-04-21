37e73c8e847bbe090dd62fac0d6747bc0a2ba356
luqxus <lebelwane@protonmain.com>
2024-04-22 01:29:53 +0200

    buy card success.

--- packages/react-app/pages/buy.tsx
+++ packages/react-app/pages/buy.tsx
@@ -5,9 +5,10 @@ import { v4 as uuidv4 } from 'uuid';
 import { parseEther } from "viem";
 import { useRouter } from "next/router";
 import { parseAccount } from "viem/utils";
+import Link from "next/link";
 const Buy = () => {
 	const account = useAccount();
-	const { data, error, isError, writeContract } = useWriteContract();
+	const { data, error, isError, isSuccess, writeContract } = useWriteContract();
 	let [amount, setAmount] = useState(0);
 	const router = useRouter();
 
@@ -30,6 +31,30 @@ const Buy = () => {
 			value: parseEther(`${amount}`)
 
 		})
+	}
+
+	if (isSuccess) {
+		return (
+			<div className="bg-gray-100 h-screen">
+				<div className="bg-white p-6  md:mx-auto">
+					<svg viewBox="0 0 24 24" className="text-green-600 w-16 h-16 mx-auto my-6">
+						<path fill="currentColor"
+							d="M12,0A12,12,0,1,0,24,12,12.014,12.014,0,0,0,12,0Zm6.927,8.2-6.845,9.289a1.011,1.011,0,0,1-1.43.188L5.764,13.769a1,1,0,1,1,1.25-1.562l4.076,3.261,6.227-8.451A1,1,0,1,1,18.927,8.2Z">
+						</path>
+					</svg>
+					<div className="text-center">
+						<h3 className="md:text-2xl text-base text-gray-900 font-semibold text-center">Card Purchase Done!</h3>
+						<p className="text-gray-600 my-2">Thank you for completing your secure online payment.</p>
+
+						<div className="py-10 text-center">
+							<Link href="/" className="px-12 bg-indigo-600 hover:bg-indigo-500 text-white font-semibold py-3">
+								GO BACK
+							</Link>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
 	}
 
 	const cancel = () => {
