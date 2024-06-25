5cdc069b9f18baaa35c108f808f9d13c5010826a
Sihle (Bafo) Shabangu <82091092+Sihle-Shabangu@users.noreply.github.com>
2024-06-25 13:27:59 +0000

    added image to navbar

--- packages/react-app/components/NavBar.tsx
+++ packages/react-app/components/NavBar.tsx
@@ -23,7 +23,7 @@ const NavBar = () => {
 				<div className="dropdown dropdown-end">
 					<div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
 						<div className="w-24 mask mask-squircle">
-							<img alt="Tailwind CSS Navbar component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+							<img alt="Tailwind CSS Navbar component" src="gift_card_landing.png" />
 						</div>
 					</div>
 					<ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
